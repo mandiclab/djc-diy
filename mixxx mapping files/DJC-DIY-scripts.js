4ae9f5aa5df7ddafeafc52dff1412463a65f03bb
mandiclab <projectmandic@gmail.com>
2026-02-03 15:33:35 +0100

    (mapping files) add author and description

--- mixxx mapping files/DJC-DIY-scripts.js
+++ mixxx mapping files/DJC-DIY-scripts.js
@@ -2,6 +2,5 @@ function DJCDIY() {}
 
 DJCDIY.wheelTurn = function (channel, control, value, status, group) {
     var newValue=(value-64);
-    engine.setValue(group, "jog", newValue)   
-    engine.scratchTick(1,newValue);
+    engine.setValue(group, "jog", newValue);   
 }
